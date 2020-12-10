ca276fe41b945a76c040adf84bf73cb2fa301d5d
shri0915 <shrijit.panicker@gmail.com>
2020-12-10 22:43:00 +0530

    Add project files.

---
+++ CompanyManagementDataLayer/CompanyManagement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementDataLayer
+{
+    public class CompanyManagement
+    {
+        DataClasses1DataContext dc = new DataClasses1DataContext();
+
+        public List<Project> GetAllProjects()
+        {
+
+            List<Project> ListOfProjects = (from Project in dc.Projects select Project).ToList();
+            return ListOfProjects;
+        }
+        public List<Technology> GetAllTechnologies()
+        {
+            List<Technology> ListOfTechnologies = (from Technology in dc.Technologies select Technology).ToList();
+            return ListOfTechnologies;
+        }
+
+        public int GetEmployeeCountForProject(int projectID)
+        {
+            int CountOfEmployeesOfProject = (from EmployeeProject in dc.EmployeeProjects where EmployeeProject.ProjectID == projectID select EmployeeProject).Count();
+            return CountOfEmployeesOfProject;
+        }
+
+        public List<Employee> GetAllEmployeesForProject(int projectID)
+        {
+            List<Employee> ListOfEmployeeFromProject = (from EmployeeProject in dc.EmployeeProjects where EmployeeProject.ProjectID == projectID select EmployeeProject.Employee).ToList();
+            return ListOfEmployeeFromProject;
+        }
+
+        public List<Project> GetAllDelayedProjects()
+        {
+            List<Project> ListOfDelayedProjects = (from Project in dc.Projects where Project.ProjectStatus == 4 select Project).ToList();
+            return ListOfDelayedProjects;
+        }
+
+        public List<Project> GetAllProjectsForEmployee(int employeeID)
+        {
+            List<Project> ListOfProjectForEmployee = (from EmployeeProject in dc.EmployeeProjects where EmployeeProject.EmployeeID == employeeID select EmployeeProject.Project).ToList();
+            return ListOfProjectForEmployee;
+        }
+
+        public List<Task> GetAllTasksForEmployee(int employeeID)
+        {
+            List<Task> ListOfTasksForEmployee = (from EmployeeTask in dc.EmployeeTasks where EmployeeTask.EmployeeID == employeeID select EmployeeTask.Task).ToList();
+            return ListOfTasksForEmployee;
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompanyManagementDataLayer;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompanyManagement c1 = new CompanyManagement();
+
+            //List all the Projects
+            foreach (var project in c1.GetAllProjects())
+            {
+                Console.WriteLine(project.ProjectID + " " + project.ProjectName + " is assigned to Department : " + project.Department.DepartmentName);
+            }
+
+            //List all the Technologies
+            foreach (var technology in c1.GetAllTechnologies())
+            {
+                Console.WriteLine(technology.TechnologyID + " " + technology.TechnologyName);
+            }
+
+            //Print the count of Employees in Project
+            Console.WriteLine(c1.GetEmployeeCountForProject(2));
+
+            //List all the Employees in the Project
+            foreach (var employeeForProject in c1.GetAllEmployeesForProject(2))
+            {
+                Console.WriteLine(employeeForProject.EmployeeID + " " + employeeForProject.EmployeeName);
+            }
+
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
