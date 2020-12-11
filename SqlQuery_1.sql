64bf42473496a35d3ca99f5ca0821c9d84194285
shri0915 <shrijit.panicker@gmail.com>
2020-12-11 13:33:07 +0530

    Populated StatusMaster for testing the insert operation

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,3 +1,7 @@
 ﻿use Company_2;
 select * from TaskTechnology;
 select * from EmployeeTask;
+select * from StatusMaster;
+select * from Project;
+insert into StatusMaster (StatusID, StatusName)
+values(1,'Active'),(2,'Assigned'),(3,'NotAssigned'),(4,'Delayed');
