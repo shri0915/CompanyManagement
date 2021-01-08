d317fd5f71ff9f5f0d984fdcb2eecbe2eab5ecbc
shri0915 <shrijit.panicker@gmail.com>
2021-01-08 12:45:55 +0530

    Started the API part of the project

---
+++ CompanyManagementAPI/Controllers/CompanyManagementController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CompanyManagementBL;
+using CompanyManagementBL.Entities;
+
+
+namespace CompanyManagementAPI.Controllers
+{
+    public class CompanyManagementController : ApiController
+    {
+        private BusinessLayer businessLayer;
+
+        public CompanyManagementController()
+        {
+            this.businessLayer = new BusinessLayer();
+        }
+
+        public List<BOProject> GetAllProjects()
+        {
+            return this.businessLayer.GetAllProjects();
+        }
+    }
+}
