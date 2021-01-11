a9b5a02a8c624b02f49beedabcc0477db391684a
shri0915 <shrijit.panicker@gmail.com>
2021-01-11 11:42:38 +0530

    Changed the controller name. Also changed the creation of BL object.

---
+++ CompanyManagementAPI/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CompanyManagementBL;
+using CompanyManagementBL.Entities;
+
+
+namespace CompanyManagementAPI.Controllers
+{
+    public class ProjectController : ApiController
+    {
+        
+
+        public List<string> GetAllProjects()
+        {
+            BusinessLayer businessLayer = new BusinessLayer();
+            List<string> projectNames = new List<string>();
+            foreach (BOProject project in businessLayer.GetAllProjects())
+            {
+                projectNames.Add(project.ProjectName);
+            }
+            return projectNames;
+        }
+    }
+}
